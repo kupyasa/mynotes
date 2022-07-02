bd00913d000b7987ef9eeb7aba5f0175173848e9
Yakup Yaşa <yakupyasa1@gmail.com>
2022-07-03 01:37:24 +0300

    step-7

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -2,8 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
-import 'dart:developer' as devtools show log;
 import '../firebase_options.dart';
+import '../utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -64,18 +64,44 @@ class _RegisterViewState extends State<RegisterView> {
               final password = _password.text;
 
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email, password: password);
-                devtools.log(userCredential.toString());
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+
+                Navigator.of(context).pushNamed(
+                  verifyEmailRoute,
+                );
               } on FirebaseAuthException catch (e) {
                 if (e.code == "weak-password") {
-                  devtools.log("Weak password");
+                  await showErrorDialog(
+                    context,
+                    "Weak password",
+                  );
                 } else if (e.code == "email-already-in-use") {
-                  devtools.log("Email already in use");
+                  await showErrorDialog(
+                    context,
+                    "Email already in use",
+                  );
                 } else if (e.code == "invalid-email") {
-                  devtools.log("Invalid email");
+                  await showErrorDialog(
+                    context,
+                    "Invalid email",
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    "Error: ${e.code}",
+                  );
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  "Error: ${e.toString()}",
+                );
               }
             },
             child: const Text("Register"),
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -15,16 +16,32 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       appBar: AppBar(
         title: const Text("Register"),
       ),
-      body: Column(
-        children: [
-          const Text("Please verify your email address"),
-          TextButton(
+      body: Center(
+        child: Column(
+          children: [
+            const Text(
+                "We've sent you email verification. Please open it to verify your account."),
+            const Text(
+                "If you haven't received verification email yet, press the button below."),
+            TextButton(
               onPressed: () async {
                 final user = FirebaseAuth.instance.currentUser;
                 await user?.sendEmailVerification();
               },
-              child: const Text("Send Email Verification"))
-        ],
+              child: const Text("Send Email Verification"),
+            ),
+            TextButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  registerRoute,
+                  (route) => false,
+                );
+              },
+              child: const Text("Restart"),
+            ),
+          ],
+        ),
       ),
     );
   }
