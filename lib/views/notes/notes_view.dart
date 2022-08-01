bd8544062f61312bee9b9f85ebd9f20cdcf7047c
Yakup Yaşa <yakupyasa1@gmail.com>
2022-08-02 00:53:17 +0300

    step-23

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/enums/menu_action.dart';
 import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/bloc/auth_bloc.dart';
+import 'package:mynotes/services/bloc/auth_event.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
 import 'package:mynotes/services/crud/notes_service.dart';
@@ -46,9 +49,9 @@ class _NotesViewState extends State<NotesView> {
                   final shouldLogOut = await showLogoutDialog(context);
                   devtools.log(shouldLogOut.toString());
                   if (shouldLogOut) {
-                    await AuthService.firebase().logOut();
-                    Navigator.of(context)
-                        .pushNamedAndRemoveUntil(loginRoute, (route) => false);
+                    context.read<AuthBloc>().add(
+                          const AuthEventLogOut(),
+                        );
                   }
                   break;
               }
